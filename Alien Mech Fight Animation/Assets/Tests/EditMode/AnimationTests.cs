aedcc2d013e4187bf529a139bfc81641fab0a396
Andrew Allbright <aallbrig@gmail.com>
2021-05-02 18:56:19 -0400

    The Animation prefab has associated timeline

---
+++ Alien Mech Fight Animation/Assets/Tests/EditMode/AnimationTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace Tests.EditMode
+{
+    public class AnimationTests
+    {
+        [Test]
+        public void Prefab_PlayableDirector_HasPlayableBound()
+        {
+            var prefab = Resources.Load<GameObject>("Prefabs/TheAnimation");
+            var gameObject = GameObject.Instantiate(prefab);
+            var playableDirector = gameObject.GetComponent<PlayableDirector>();
+
+            Assert.NotNull(playableDirector.playableAsset);
+        }
+    }
+}
