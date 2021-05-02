2ced525cb4d5f11dee87d682c8a7a292702e9295
Andrew Allbright <aallbrig@gmail.com>
2021-05-02 19:08:10 -0400

    Environment has 100 x 100 ground

---
+++ Alien Mech Fight Animation/Assets/Tests/EditMode/EnvironmentTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Tests.EditMode
+{
+    public class EnvironmentTests
+    {
+        [Test]
+        public void Environment_IsStatic()
+        {
+            var prefab = Resources.Load<GameObject>("Prefabs/Environment");
+            var gameObject = GameObject.Instantiate(prefab);
+            Assert.IsTrue(gameObject.isStatic);
+        }
+
+        [Test]
+        public void Environment_HasGround()
+        {
+            var prefab = Resources.Load<GameObject>("Prefabs/Environment");
+            var gameObject = GameObject.Instantiate(prefab);
+            var ground = gameObject.transform.Find("Ground");
+
+            Assert.NotNull(ground);
+        }
+    }
+}
